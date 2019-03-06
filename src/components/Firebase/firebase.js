cbf63821c684c3ecdc520c6eddd0c22619deafed
Joseph Huang <joseph@streetbees.com>
2019-03-06 17:02:46 +0000

    updated firebase js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
 
@@ -7,7 +8,24 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
+    this.googleProvider = new app.auth.GoogleAuthProvider();
   }
+
+  doCreateUserWithEmail = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmail = (email, password) =>
+    this.auth.signInUserWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  doSignInWithGoogle = () =>
+    this.auth.signInWithPopup(this.googleProvider);
 }
 
 export default Firebase;
